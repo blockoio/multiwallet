4049c2b5069d2dfe966f821f64209e103d413048
findmytrueself <load1999@gmail.com>
2024-12-06 16:23:20 +0900

    update: payload type

--- dapp/js/util.js
+++ dapp/js/util.js
@@ -1,72 +1,82 @@
-import { AergoClient, GrpcWebProvider } from "@herajs/client";
+import { AergoClient, GrpcWebProvider } from '@herajs/client'
 
 export function sendTxByAergoConnect(endpoint, contractID, payload) {
-    let herajs = new AergoClient({}, new GrpcWebProvider({ url: endpoint }));
+  let herajs = new AergoClient({}, new GrpcWebProvider({ url: endpoint }))
 
-    var builtTx = {
-        to: contractID,
-        amount: "0",
-        payload_json: payload
-    }
+  var builtTx = {
+    to: contractID,
+    amount: '0',
+    payload_json: payload,
+    type: 5,
+  }
+  console.log(payload, 'payload')
 
-    return new Promise((resolve, reject) => {
-        try {
-            // register event handler
-            window.addEventListener("AERGO_SEND_TX_RESULT", (event) => {
-                setTimeout(async () => {
-                    try {
-                        if (event.detail.error) {
-                            reject(event.detail.error);
-                        } else {
-                            const receipt = await herajs.getTransactionReceipt(
-                                event.detail.hash
-                            );
-                            resolve(receipt);
-                        }
-                    } catch (err) {
-                        reject(err);
-                    }
-                }, 2000);
-            }, {
-                once: true
-            });
-            // send build tx request
-            window.postMessage({
-                type: "AERGO_REQUEST",
-                action: "SEND_TX",
-                data: builtTx
-            });
-        } catch (err) {
-            reject(err);
+  return new Promise((resolve, reject) => {
+    try {
+      // register event handler
+      window.addEventListener(
+        'AERGO_SEND_TX_RESULT',
+        (event) => {
+          setTimeout(async () => {
+            try {
+              if (event.detail.error) {
+                reject(event.detail.error)
+              } else {
+                const receipt = await herajs.getTransactionReceipt(
+                  event.detail.hash
+                )
+                resolve(receipt)
+              }
+            } catch (err) {
+              reject(err)
+            }
+          }, 2000)
+        },
+        {
+          once: true,
         }
-    });
+      )
+      // send build tx request
+      window.postMessage({
+        type: 'AERGO_REQUEST',
+        action: 'SEND_TX',
+        data: builtTx,
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
 // request sign to aergo connect
 export function signByAergoConnect(message) {
-    return new Promise((resolve, reject) => {
-        try {
-            // register event handler
-            window.addEventListener("AERGO_SIGN_RESULT", (event) => {
-                if (event.detail.error) {
-                    reject(event.detail.error);
-                } else {
-                    resolve(event.detail.signature);
-                }
-            }, {
-                once: true
-            });
-
-            // send sign message request
-            window.postMessage({
-                type: "AERGO_REQUEST",
-                action: "SIGN",
-                data: { hash: message }
-            });
-        } catch (err) {
-            reject(err);
+  return new Promise((resolve, reject) => {
+    try {
+      // register event handler
+      window.addEventListener(
+        'AERGO_SIGN_RESULT',
+        (event) => {
+          if (event.detail.error) {
+            reject(event.detail.error)
+          } else {
+            resolve(event.detail.signature)
+          }
+        },
+        {
+          once: true,
         }
-    });
+      )
+
+      // send sign message request
+      window.postMessage({
+        type: 'AERGO_REQUEST',
+        action: 'SIGN',
+        data: { hash: message },
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
-export default {};
+export default {}
